3d2fc3516b8f44bbc17d1fcdff2e685662915a83
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-12-27 22:11:46 +0200

    useState

---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import styles from './ColorPicker.module.css';
+
+export default function ColorPicker({ options }) {
+  const [activeOptionIdx, setActiveOptionIdx] = useState(0);
+  const { label } = options[activeOptionIdx];
+
+  const makeOptionClassName = index => {
+    return index === activeOptionIdx ? styles.activeOption : styles.option;
+  };
+
+  //   state = {
+  //     activeOptionIdx: 0,
+  //   };
+
+  //   setActiveIdx = index => {
+  //     this.setState({ activeOptionIdx: index });
+  //   };
+
+  //   makeOptionClassName = index => {
+  //     return index === this.state.activeOptionIdx
+  //       ? styles.activeOption
+  //       : styles.option;
+  //   };
+
+  // const { activeOptionIdx } = this.state;
+  // const { label } = options[activeOptionIdx];
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Color Picker</h2>
+      <p>Выбран цвет: {label}</p>
+      <div>
+        {options.map(({ label, color }, index) => (
+          <button
+            key={label}
+            aria-label={label}
+            className={makeOptionClassName(index)}
+            style={{ backgroundColor: color }}
+            onClick={() => setActiveOptionIdx(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
